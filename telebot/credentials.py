9c86bafc6b803bbd28abbfe2e0846e170950447b
shadeshade <privetabdul@gmail.com>
2020-06-13 20:43:13 +0300

    first commit

---
+++ telebot/credentials.py
@@ -0,0 +1,3 @@
+bot_token = '1100494411:AAGAiZnnrP-up2YcDXQEJ73R4LNGUiLeomI'
+bot_user_name = "echotesttest123bot"
+URL = "https://git.heroku.com/shade-weather-telegram-bot.git"
---
+++ telebot/mastermind.py
@@ -0,0 +1,7 @@
+def get_response(msg):
+    """
+    you can place your mastermind AI here
+    could be a very basic simple response like "ok"
+    or a complex LSTM network that generate appropriate answer
+    """
+    return "ok"
