7a8c948d320acdabaf265e2116bcff1da5084057
shadeshade <privetabdul@gmail.com>
2020-07-04 13:24:49 +0300

    Moved parse scripts into project Moved cities_bd file into separate folder Corrected path to cities_bd file

--- telebot/mastermind.py
+++ telebot/mastermind.py
@@ -1,9 +1,15 @@
+import os
+
 import transliterate
 import json
 from bs4 import BeautifulSoup
 import requests
 
-with open('cities_bd.json', 'r', encoding='utf-8') as f:
+CUR_PATH = os.path.realpath(__file__)
+BASE_DIR = os.path.dirname(os.path.dirname(CUR_PATH))
+DATA_DIR = os.path.join(BASE_DIR, 'data')
+
+with open(os.path.join(DATA_DIR, 'cities_bd.json'), 'r', encoding='utf-8') as f:
     data = json.load(f)
 
 
@@ -27,3 +33,7 @@ def get_response(city):
     temp = soup.find('div', class_='fact__temp-wrap')
     temp = temp.find(class_='temp__value').text
     return temp
+
+
+if __name__ == '__main__':
+    print(get_response('ТэджоН'))
