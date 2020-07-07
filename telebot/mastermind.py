2c61667966f0468f4063b37f67905381c519ac43
shadeshade <privetabdul@gmail.com>
2020-07-07 13:00:25 +0300

    updated get_response func

--- telebot/mastermind.py
+++ telebot/mastermind.py
@@ -1,9 +1,15 @@
+import os
+
 import transliterate
 import json
 from bs4 import BeautifulSoup
 import requests
 
-with open('cities_bd.json', 'r', encoding='utf-8') as f:
+CUR_PATH = os.path.realpath(__file__)
+BASE_DIR = os.path.dirname(os.path.dirname(CUR_PATH))
+DATA_DIR = os.path.join(BASE_DIR, 'data')
+
+with open(os.path.join(DATA_DIR, 'cities_bd.json'), 'r', encoding='utf-8') as f:
     data = json.load(f)
 
 
@@ -20,10 +26,17 @@ def translit_name(name):
         return new_name
 
 
-def get_response(city):
-    city = translit_name(city)
-    source = requests.get('https://yandex.ru/pogoda/' + city)
-    soup = BeautifulSoup(source.content, 'lxml')
-    temp = soup.find('div', class_='fact__temp-wrap')
-    temp = temp.find(class_='temp__value').text
+def get_response(city_name):
+    city = translit_name(city_name)
+    try:
+        source = requests.get('https://yandex.ru/pogoda/' + city)
+        soup = BeautifulSoup(source.content, 'lxml')
+        temp = soup.find('div', class_='fact__temp-wrap')
+        temp = temp.find(class_='temp__value').text
+    except:
+        temp = 'Try again'
     return temp
+
+
+if __name__ == '__main__':
+    print(get_response('moscow'))
