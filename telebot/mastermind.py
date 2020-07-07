6af7a40a3b7d21eefd57e57f8724f0990b894876
shadeshade <privetabdul@gmail.com>
2020-07-07 15:13:22 +0300

    checking heroku server

--- telebot/mastermind.py
+++ telebot/mastermind.py
@@ -5,31 +5,37 @@ import json
 from bs4 import BeautifulSoup
 import requests
 
-# CUR_PATH = os.path.realpath(__file__)
-# BASE_DIR = os.path.dirname(os.path.dirname(CUR_PATH))
-# DATA_DIR = os.path.join(BASE_DIR, 'data')
-#
-# with open(os.path.join(DATA_DIR, 'cities_bd.json'), 'r', encoding='utf-8') as f:
-#     data = json.load(f)
+CUR_PATH = os.path.realpath(__file__)
+BASE_DIR = os.path.dirname(os.path.dirname(CUR_PATH))
+DATA_DIR = os.path.join(BASE_DIR, 'data')
 
-#
-# def translit_name(name):
-#     if name.title() in data:
-#         return data[name.title()]
-#     else:
-#         try:
-#             new_name = transliterate.translit(name, reversed=True)
-#             if 'х' in name.lower():
-#                 new_name = new_name.lower().replace('h', 'kh')
-#         except:
-#             new_name = name
-#         return new_name
+with open(os.path.join(DATA_DIR, 'cities_bd.json'), 'r', encoding='utf-8') as f:
+    data = json.load(f)
+
+
+def translit_name(name):
+    if name.title() in data:
+        return data[name.title()]
+    else:
+        # try:
+        #     new_name = transliterate.translit(name, reversed=True)
+        #     if 'х' in name.lower():
+        #         new_name = new_name.lower().replace('h', 'kh')
+        # except:
+        #     new_name = name
+        return name
 
 
 def get_response(city_name):
-
-
-    return 'ok'
+    city = translit_name(city_name)
+    try:
+        source = requests.get('https://yandex.ru/pogoda/' + city)
+        soup = BeautifulSoup(source.content, 'lxml')
+        temp = soup.find('div', class_='fact__temp-wrap')
+        temp = temp.find(class_='temp__value').text
+    except:
+        temp = 'Try again'
+    return temp
 
 
 if __name__ == '__main__':
