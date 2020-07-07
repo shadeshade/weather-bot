e5eb4d20936c9b92bd2691b7ef0010b13d14ae48
shadeshade <privetabdul@gmail.com>
2020-07-07 13:30:44 +0300

    /

--- telebot/mastermind.py
+++ telebot/mastermind.py
@@ -13,21 +13,21 @@ with open(os.path.join(DATA_DIR, 'cities_bd.json'), 'r', encoding='utf-8') as f:
     data = json.load(f)
 
 
-def translit_name(name):
-    if name.title() in data:
-        return data[name.title()]
-    else:
-        try:
-            new_name = transliterate.translit(name, reversed=True)
-            if 'х' in name.lower():
-                new_name = new_name.lower().replace('h', 'kh')
-        except:
-            new_name = name
-        return new_name
+# def translit_name(name):
+#     if name.title() in data:
+#         return data[name.title()]
+#     else:
+#         try:
+#             new_name = transliterate.translit(name, reversed=True)
+#             if 'х' in name.lower():
+#                 new_name = new_name.lower().replace('h', 'kh')
+#         except:
+#             new_name = name
+#         return new_name
 
 
-def get_response(city_name):
-    city = translit_name(city_name)
+def get_response(city):
+    # city = translit_name(city_name)
     try:
         source = requests.get('https://yandex.ru/pogoda/' + city)
         soup = BeautifulSoup(source.content, 'lxml')
