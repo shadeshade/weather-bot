89efceb3fcd3898933d6c1caae4e3e536ba6ea28
shadeshade <privetabdul@gmail.com>
2020-07-20 14:43:56 +0300

    checking server

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,4 @@
 TOKEN = '1100494411:AAGAiZnnrP-up2YcDXQEJ73R4LNGUiLeomI'
 BOT_NAME = "echotesttest123bot"
-DEPLOY_DOMAIN = "shade-weather-telegram-bot.herokuapp.com"
+# DEPLOY_DOMAIN = "shade-weather-telegram-bot.herokuapp.com"
+DEPLOY_DOMAIN = "sleepy-lowlands-22859.herokuapp.com"
