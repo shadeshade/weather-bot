bc2c5553fecadf0dde0a8fc3a9a10716d3bde6d4
shadeshade <privetabdul@gmail.com>
2020-11-13 16:42:35 +0300

    Deploy

--- app/__init__.py
+++ app/__init__.py
@@ -24,5 +24,5 @@ logging.basicConfig(filename=os.path.join(BASE_DIR, 'log.log'), level=logging.DE
 logger = logging.getLogger()
 
 
-# def get_app():
-#     return app
+def get_app():
+    return app
--- run.py
+++ run.py
@@ -1,9 +1,9 @@
-from app import app
+from app import app, get_app
 from app.mastermind.scheduling import back_up_reminders
 from app.views import set_webhook
 from app.credentials import SERVER_IP, PORT, DEBUG
 
-# server = get_app()
+server = get_app()
 
 if __name__ == '__main__':
     set_webhook()
