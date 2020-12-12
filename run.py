d1715476b43c93719ce11d5572ee9761c46eacbd
shadeshade <privetabdul@gmail.com>
2020-12-12 20:44:12 +0300

    Change requirements, Use build in job storage, Specify jobstore, Added timezone and start call for scheduler

--- run.py
+++ run.py
@@ -1,16 +1,7 @@
 from app import server
 from app.credentials import SERVER_IP, PORT, DEBUG
-from app.mastermind.scheduling import back_up_reminders
-from app.views import set_webhook
 
 run_app = server
-# def run_app(*args):
-    # back_up_reminders()
-    # set_webhook()
-    # return server
 
-#
-# if __name__ == '__main__':
-#     set_webhook()
-#     back_up_reminders()
-#     server.run(threaded=True, host=SERVER_IP, port=PORT, debug=DEBUG, use_reloader=False)
+if __name__ == '__main__':
+    server.run(threaded=True, host=SERVER_IP, port=PORT, debug=DEBUG, use_reloader=False)
